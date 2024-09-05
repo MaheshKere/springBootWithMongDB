45d5acbc58f4c84a5b5494f8dc643db32f83b810
Mahesh Kere <mahesh.kere@netcracker.com>
2024-09-05 18:51:51 +0530

    Studentcontrolle

--- src/main/java/com/mangocrud/crud/restcontroller/Studentcontroller.java
+++ src/main/java/com/mangocrud/crud/restcontroller/Studentcontroller.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * StudentRestController
+ */
 @RestController
 public class Studentcontroller {
 
